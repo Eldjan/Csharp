569bdc9d331d44483add0e8b851d8b7b4d8262e7
Eljan <elcan.elizade.2004@gmail.com>
2022-05-27 23:57:00 -0700

    Task 1

---
+++ Ededin vurma cedveli/Ededin vurma cedveli.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ededin vurma cedveli
+
+            Console.WriteLine("Enter the number");
+            
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine(i + "x" + a + "=" + (i*a));
+            }
+            Console.ReadLine();
+        }
+    }
+}
