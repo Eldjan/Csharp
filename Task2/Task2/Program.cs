fce5716f15df2953ce20f37ff04c5d59257935a5
Eljan <elcan.elizade.2004@gmail.com>
2022-06-02 06:29:52 -0700

    Task

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 ile verilmis ededin kvadratlari cemi
+
+            Console.WriteLine("Enter the number:");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+         
+            for (int i = 1; i <= a; i++)
+            {
+                sum += i * i;   
+            }
+            Console.WriteLine("Result:\t" + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks lesson 4/Tasks lesson 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_lesson_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] array= { 9,3,1,4,2,5,7,6,8,12 };
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        n = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = n;
+                    }
+                }
+            }
+            Console.WriteLine(array[0]);
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
