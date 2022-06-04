ce99e782e7b07c28d6cd7654a0ba84ab3d0b02c0
Eljan <elcan.elizade.2004@gmail.com>
2022-06-04 02:31:09 -0700

    Task

---
+++ Age/Age/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yasiniz: ");
+            long y = Convert.ToInt64(Console.ReadLine());
+            if (y < 30 && y >= 0)
+            {
+                Console.WriteLine(y * y);
+            }
+            else if (y >= 30 && y <= 40)
+            {
+                Console.WriteLine(y % 10);
+            }
+            else
+            {
+                Console.WriteLine("Düzgün melumat daxil etmemisiniz");
+            }
+        }
+    }
+}
