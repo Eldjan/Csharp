4fbf5a8253056a04eda536b10d2a43f8952d0f07
Eljan <elcan.elizade.2004@gmail.com>
2022-06-05 10:17:28 -0700

    teacher task 2

---
+++ 4 digit number/4 digit number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_digit_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SumNumbers(1298);
+
+        }
+        public static void SumNumbers(int a)
+        {
+            
+            int resault = 0;
+
+            for (int i = 1; i <= 1000; i *= 10)
+            {
+                int b = a /i;
+                int c = b % 10;
+                resault = c+resault;
+            }
+            Console.WriteLine(resault);
+        }
+    }
+
+}
--- Methods/4 digit number.cs
+++
@@ -1,4 +0,0 @@
-﻿Build started...
-1>------ Build started: Project: 4 digit number, Configuration: Debug Any CPU ------
-1>  4 digit number -> C:\Users\hp\Desktop\github3\4 digit number\4 digit number\bin\Debug\4 digit number.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
