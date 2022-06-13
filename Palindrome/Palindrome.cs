46aa0464a3ae82a6dbc609762d51708636f77327
Eljan <elcan.elizade.2004@gmail.com>
2022-06-13 10:05:09 -0700

    teacher task

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,4 @@
+﻿Build started...
+1>------ Build started: Project: Palindrome task, Configuration: Debug Any CPU ------
+1>  Palindrome task -> C:\Users\hp\Desktop\github3\Palindrome task\Palindrome task\bin\Debug\Palindrome task.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
