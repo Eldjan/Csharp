94af9a7ee013d1c776b56b4b76db8c79625844e5
Eljan <elcan.elizade.2004@gmail.com>
2022-06-13 07:01:27 -0700

    teacher task

---
+++ 4 digit number/Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_digit_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void SumNumbers(int a)
+        {
+            
+            int count = a.ToString().Length;
+            int[] arr= new int[count];
+            int index = 0;
+            for (int i = 1; i <= Length(count); i *= 10)
+            {
+                int b = a / i;
+                int c = b % 10;
+                arr[index] = c;
+                index++;
+            }
+            Console.WriteLine(resault);
+        }
+
+        public static int Length(int count)
+        {
+            int length = 1;
+            for (int i = 0; i < count; i++)
+            {
+                length *= 10;
+            }
+            return length;
+        }
+
+
+
+    }
+}
+
+
---
+++ CalculationArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation(1, 2, 3,4);
+        }
+        public static void Calculation(params int[] arr)
+        {
+            int sum = 0;
+            int mul = 1;
+            int res = 0;
+            foreach(var item in arr)
+            {
+                sum += item;
+                mul *= item;
+            }
+            res = mul - sum;
+            Console.WriteLine( res );
+
+
+        }
+        
+        
+    }
+}
