8855a61c8f4d52ac7d31e1a0ed7f6c83d8c27e1f
Eljan <elcan.elizade.2004@gmail.com>
2022-06-19 10:18:42 -0700

    teacher task

---
+++ IsPrime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine( IsPrime(19));
+        }
+        public static bool IsPrime(int x)
+        {
+            for(int i = 2; i < x/2+1; i++)
+            {
+                if(x%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
