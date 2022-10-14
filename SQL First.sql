f2549c9b55cd67fddcd0819745bf8d4d90bb59e3
Eljan <105805047+Eldjan@users.noreply.github.com>
2022-10-14 08:17:35 -0700

    teacher task

---
+++ SQL First.sql
@@ -0,0 +1,47 @@
+use Academy
+
+--CREATE TABLE Student
+--(
+--Id int identity(1,50),
+--[Grade] int,
+--Fullname nvarchar(50),
+--[Group] nvarchar(20),
+--Birthday date
+--);
+
+--select *
+--from Student
+--where grade>9 AND [Group]<2;
+
+--select *
+--from Student
+--where grade>9 or [Group]<2;
+
+--select
+--Grade, 
+--fullname
+--from Student
+--where Grade in (11);
+
+--select
+--Grade, 
+--fullname
+--from Student
+--where grade between 9 and 11 ;
+
+--select
+--Grade, 
+--fullname
+--from Student
+--where Fullname like 'Ay%';
+
+--select
+--Grade, 
+--fullname
+--from Student
+--Order by Grade Desc
+--OFFSET 0 ROWS 
+--FETCH FIRST 10 ROWS ONLY;
+
+
+
